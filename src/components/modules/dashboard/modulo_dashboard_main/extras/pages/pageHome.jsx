62ddc32f642c336430b41c549abd70b1240d40de
johanVillamizarCode <johanmauricio218@gmail.com>
2023-11-29 01:30:26 -0500

    Se agregan algunas paginas funcionales

---
+++ src/components/modules/dashboard/modulo_dashboard_main/extras/pages/pageHome.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const PageHome = () => {
+  return (
+    <div className="col-md-12">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card">
+            <div className="bar-container">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PageHome;
---
+++ src/components/modules/dashboard/modulo_dashboard_main/extras/pages/pageInformacion.jsx
@@ -0,0 +1,60 @@
+const PageInformacion = () => (
+    <div className="col-md-12">
+    <div className="row">
+        <div className="col-md-12">
+            <div className="card">
+                <h2>Bienvenidos a AcaForms APP</h2><br />
+                <p> 
+                    <b>Estado:</b> Prototipo Alfa <br />
+                    <b>Versión:</b> 1.0.0a <br />
+                    <b>Desarrollador:</b> Johan Mauricio Chia Villamizar - Estudiante de Ingeniería de Sistemas, UDI <br /><br />
+
+                    AcaForms APP, facilita la creación de encuestas y formularios con una perspectiva académica. nuestra aplicación se encuentra actualmente en la fase Alfa. <br />
+                    <br />
+                    <b>Características Actuales: </b><br />
+                    <br />
+                    <ul>
+                        <li>Interfaz intuitiva para la creación rápida de encuestas.</li>
+                        <li>Personalización de preguntas y respuestas.</li>
+                        <li>Enfoque inicial en las necesidades académicas.</li>
+                    </ul>
+                    Características Futuras: <br />
+
+                    <ul>
+                        <li>Ampliación a entornos empresariales y de uso general.</li>
+                        <li>Colaboraciones con otros desarrolladores para enriquecer funcionalidades.</li>
+                        <li>Mejoras continuas basadas en tus valiosos comentarios.</li>
+                    </ul>
+                    
+                    
+                    
+                    <h4>Modularidad del Código:</h4>
+                    La esencia de AcaForms APP reside en la modularidad de su código, una característica central que proporciona beneficios significativos: <br /><br />
+
+                    <b>Reutilización de Componentes: </b>
+
+                    Facilita la reutilización eficiente de componentes en diferentes áreas de la aplicación, fomentando la coherencia y reduciendo la redundancia de código. <br />
+                    <br /><b>Facilidad de Mantenimiento: </b>
+
+                    Permite una identificación y corrección de errores más rápida, así como la introducción sencilla de nuevas funcionalidades sin afectar otras partes del sistema. <br />
+                    <br /><b>Escalabilidad: </b>
+
+                    Prepara el terreno para la expansión futura, posibilitando la incorporación sencilla de nuevas características y adaptándose a los requisitos cambiantes. <br />
+                    <br /><b>Desarrollo Colaborativo: </b>
+
+                    Facilita el trabajo colaborativo, ya que diferentes desarrolladores pueden centrarse en módulos específicos sin interferir con el trabajo de los demás. <br />
+                    <br /><b>Pruebas Unitarias Efectivas: </b>
+
+                    Permite pruebas unitarias aisladas, facilitando la detección y resolución de problemas específicos sin afectar otras áreas del sistema.
+                    Estas características resaltan el enfoque principal en la modularidad del código, una decisión que mejora la eficiencia, la mantenibilidad y la flexibilidad de AcaForms APP.  <br />
+
+                    <br />Tu participación y comentarios son esenciales mientras evolucionamos AcaForms APP. Agradecemos tu apoyo y entusiasmo mientras trabajamos para perfeccionar este prototipo.
+                    ¡Gracias por ser parte de esta emocionante fase Alfa de desarrollo!
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+);
+
+export default PageInformacion;
