d9939fab82f488a052b53f703fda43ced78f5a15
johanVillamizarCode <johanmauricio218@gmail.com>
2023-11-29 01:31:21 -0500

    Se agrega funcionalidad al menu principal lateral

--- src/components/modules/dashboard/modulo_dashboard_main/ModuloPrincipalContainer.jsx
+++ src/components/modules/dashboard/modulo_dashboard_main/ModuloPrincipalContainer.jsx
@@ -1,51 +1,61 @@
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ModuloComponent from "../modulo_dashboard_componentePrincipal/ModuloComponent";
 import ModuloPrincipalContainer_Contador from "../modulo_dashboard_main_estadisticas/ModuloPrincipalContainer_Contador";
+
+// === Imports del MenuLateral ===
 import MenuLateral from "./extras/menuLateral";
+import PageInformacion from "./extras/pages/pageInformacion";
+import PageHome from './extras/pages/pageHome';
+// =======================================================
 
-const ModuloPrincipalContainer = () =>(
-  // Sin funcionar
-    const [componenteActual, setComponenteActual] = useState(<Inicio />);
-
-  const manejarClicEnMenu = (opcion) => {
+const ModuloPrincipalContainer = () => {
+  const [contenido, setContenido] = useState(null);
+  
+  const handleMenuClick = (opcion) => {
     switch (opcion) {
-      case 'Inicio':
-        setComponenteActual(<Inicio />);
+      case 'inicio':
+        setContenido(<PageHome />);
         break;
-      case 'CreacionEncuestas':
-        setComponenteActual(<CreacionEncuestas />);
+      case 'creacionEncuestas':
+        setContenido(<ModuloComponent />);
         break;
-      case 'Resultados':
-        setComponenteActual(<Resultados />);
+      case 'resultados':
+        // Agrega el componente correspondiente a 'resultados' si es necesario
         break;
-      case 'Configuracion':
-        setComponenteActual(<Configuracion />);
-        break;
-      case 'Salir':
-        setComponenteActual(<Salir />);
+      case 'informacion':
+        setContenido(<PageInformacion />);
         break;
       default:
-        setComponenteActual(null);
+        setContenido(null);
     }
   };
+
+  useEffect(() => {
+    // Este efecto se ejecuta al montar el componente
+    // Aquí puedes establecer el contenido inicial, por ejemplo, PageHome
+    setContenido(<PageHome />);
+  }, []);
+
+  
+  return (
     <div className="col-md-12">
-        <div className="row justify-content-center">
-            <div className="col-md-9">
-                <div className="row">
-                    <div className="col-md-3">
-                        <div className="card">
-                            <MenuLateral />
-                        </div>
-                    </div>
-                    <div className="col-md-9">
-                        <ModuloPrincipalContainer_Contador />
-                        <ModuloComponent />
-                    </div>
-                </div>
+      <div className="row justify-content-center">
+        <div className="col-md-9">
+          <div className="row">
+            <div className="col-md-3">
+              <div className="card">
+                <MenuLateral onMenuClick={handleMenuClick} />
+              </div>
             </div>
+            <div className="col-md-9">
+              <ModuloPrincipalContainer_Contador />
+              {contenido}
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-);
+  );
+};
 
 export default ModuloPrincipalContainer;
--- src/components/modules/dashboard/modulo_dashboard_main/extras/menuLateral.jsx
+++ src/components/modules/dashboard/modulo_dashboard_main/extras/menuLateral.jsx
@@ -1,12 +1,36 @@
+import PropTypes from 'prop-types';
 
-const MenuLateral = () => (
-    <ul className="list-group">
-        <li className="list-group-item">Inicio</li>
-        <li className="list-group-item">Creación de encuestas</li>
-        <li className="list-group-item">Resultados</li>
-        <li className="list-group-item">Configuración</li>
-        <li className="list-group-item list-group-item-danger">Salir</li>
+const TuMenuLateral = ({ onMenuClick }) => {
+  const handleClick = (opcion) => {
+    // Llamar a la función onMenuClick pasando la opción
+    if (onMenuClick) {
+      onMenuClick(opcion);
+    }
+  };
+
+  return (
+    <ul className="list-group TuMenuLateral_ULprincipal">
+      <li className="list-group-item" onClick={() => handleClick('inicio')} style={{ cursor: 'pointer' }}>
+        <i className="fa-solid fa-house"></i> Inicio
+      </li>
+      <li className="list-group-item" onClick={() => handleClick('creacionEncuestas')} style={{ cursor: 'pointer' }}>
+        <i className="fa-solid fa-plus"></i> Creación de encuestas
+      </li>
+      <li className="list-group-item" onClick={() => handleClick('resultados')} style={{ cursor: 'pointer' }}>
+        <i className="fa-regular fa-folder"></i> Resultados
+      </li>
+      <li className="list-group-item" onClick={() => handleClick('informacion')} style={{ cursor: 'pointer' }}>
+        <i className="fa-solid fa-circle-info"></i> Información
+      </li>
+      <li className="list-group-item list-group-item-danger" onClick={() => handleClick('salir')} style={{ cursor: 'pointer' }}>
+        <i className="fa-solid fa-xmark"></i> Salir
+      </li>
     </ul>
-);
+  );
+};
 
-export default MenuLateral;
+TuMenuLateral.propTypes = {
+  onMenuClick: PropTypes.func.isRequired,
+};
+
+export default TuMenuLateral;
